48465afbb0060b4a9da0115a8b524073ce3f328c
Kamyar Mohajerani <kammoh@gmail.com>
2022-10-18 16:13:36 -0400

    fix backup_db

--- backup_db.sh
+++ backup_db.sh
@@ -10,8 +10,11 @@ export PGPASSWORD="${DB_PASSWORD}"
 export DATABASE_URL="postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
 
 TIMESTAMP=$(date +%d-%m-%Y_%H-%M-%S)
-
 BACKUP_FILENAME=${DB_NAME}_backup_${TIMESTAMP}.tar
-pg_dump -F t ${DB_NAME} > ${BACKUP_FILENAME}
+
+echo ${DB_PASSWORD}  > .pgpass
+
+pg_dump --username ${DB_USER} --host=${DB_HOST} --port=${DB_PORT}  -b -F t ${DB_NAME} > ${BACKUP_FILENAME}
 
 echo backup of ${DB_NAME} saved to ${BACKUP_FILENAME}
+rm -f .pgpass
